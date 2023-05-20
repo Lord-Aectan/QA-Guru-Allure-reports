7ccb010eee633f68e7ad455f7cc701967d6519c4
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-20 15:06:49 +0300

    Удалил test_search_issue_without_steps.py, так как он больше не нужен

--- tests/test_search_issue_without_steps.py
+++
@@ -1,8 +0,0 @@
-from selene import browser, by, be, have
-from selene.support.shared.jquery_style import s
-
-
-def test_search_issue_without_steps(browser_start):
-    browser.open('https://github.com/Lord-Aectan/QA-Guru-Allure-reports')
-    s('#issues-tab').click()
-    s(by.partial_text('#1')).should(be.visible)
