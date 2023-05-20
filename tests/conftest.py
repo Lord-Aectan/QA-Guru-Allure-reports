ed651ead72ec28ff9ffbba226f03607c6de88790
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-20 13:24:43 +0300

    Создал requirements.txt, conftest.py и test_search_issue_without_steps.py. Написал тест без шагов в test_search_issue_without_steps.py

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from selene import browser, by, be, have
+
+
+@pytest.fixture()
+def browser_start():
+    browser.config.window_width = 1520
+    browser.config.window_height = 1080
+
+    yield
